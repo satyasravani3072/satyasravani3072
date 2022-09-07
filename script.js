44a8d2bfe6fe1fdfe60d0a885eaee7bc5d12951c
satya <112604721+satyasravani3072@users.noreply.github.com>
2022-09-07 13:43:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// $('.navTrigger').click(function() {
+//     $(this).toggleClass('active'):
+//     Console.log("clicked menu");
+//     $("#mainListDiv").toggleClass("show_list");
+//     $("#mainListDiv").fadeIn();
+// });
+
+// $(window).scroll(function() {
+//     if ($(document).scrollTop() > 50) {
+//         $('.nav').addClass('affix');
+//         console.log("OK");
+//     } else {
+//         $('.nav').removeClass('affix');
+//     }
+// });
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+
